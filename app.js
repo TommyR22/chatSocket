5810e52927b090b5f18544fe2515da08f2f8ad3f
Giuseppe Zappulla <g.zappulla@reply.it>
2018-11-29 13:28:50 +0100

    fix server + remove node_modules

--- app.js
+++ app.js
@@ -1,140 +1,148 @@
-const { Observable, fromEvent, empty, timer } = rxjs;
-const { ajax } = rxjs.ajax;
-const { webSocket } = rxjs.webSocket;
-const { debounceTime, map, switchMap, catchError, retryWhen } = rxjs.operators;
+(function () {
+    const { Observable, fromEvent, empty, timer } = rxjs;
+    const { ajax } = rxjs.ajax;
+    const { webSocket } = rxjs.webSocket;
+    const { debounceTime, map, switchMap, catchError, retryWhen } = rxjs.operators;
 
-let serverMessages = [];
+    let serverMessages = [];
 
-let message = {
-    content: '',
-    sender: '',
-    isBroadcast: '',
-    receiver: ''
-}
+    let message = {
+        type: '',
+        content: '',
+        sender: '',
+        receiver: ''
+    };
 
-const username = 'spiderman';
-const username_friend = 'yoda';
+    const username = 'spiderman';
+    const username_friend = 'yoda';
 
-let clientID;
-let viewer = document.getElementsByClassName('messages');
-let img = 'assets/image/spiderman.jpg';
-let img2 = 'assets/image/yoda.jpg';
+    let myClientID = 0;
+    let viewer = document.getElementsByClassName('messages');
+    let img = 'assets/image/spiderman.jpg';
+    let img2 = 'assets/image/yoda.jpg';
 
 // WebSocket
-const webSocket$ = webSocket('ws://localhost:8081');
-webSocket$.subscribe((message) => {
-        if (message.content === 'connected_to_server') {
-            console.log(`message received from: ${message.sender}`);
-            sendMessage('login', username, false);
-        } else if (message.content === 'new_client') {
-            updateContact(message);
-        } else {
-            console.log('message received: ', message);
-            serverMessages.push(message);
-            addMessage(message);
-            scroll();
+    const webSocket$ = webSocket('ws://localhost:8081');
+    webSocket$
+        .multiplex(
+            () => ({type: 'sub', symbol}),
+            () => ({type: 'unsub', symbol}),
+            x => x.symbol === symbol
+        )
+        .subscribe((message) => {
+                if (message.content === 'connected_to_server') {
+                    console.log(`message received from: ${message.sender}`);
+                    sendMessage('login', username, false);
+                } else if (message.content === 'new_client') {
+                    updateContact(message);
+                } else {
+                    console.log('message received: ', message);
+                    serverMessages.push(message);
+                    addMessage(message);
+                    scroll();
+                }
+            },
+            (err) => console.error(err),
+            () => console.warn('Completed!')
+        );
+
+    function sendMessage(content, sender, isBroadcast, receiver) {
+        message = {
+            content: content,
+            sender: sender,
+            isBroadcast: isBroadcast,
+            receiver: receiver
+        };
+        // serverMessages.push(message);
+        console.log('sendMessage: ', JSON.stringify(message));
+        webSocket$.next(message);
+        if (receiver) {
+            showMessage(message);
         }
-    },
-    (err) => console.error(err),
-    () => console.warn('Completed!')
-);
-
-function sendMessage(content, sender, isBroadcast, receiver){
-    message = {
-        content: content,
-        sender: sender,
-        isBroadcast: isBroadcast,
-        receiver: receiver
-    };
-    // serverMessages.push(message);
-    console.log('sendMessage: ', JSON.stringify(message));
-    webSocket$.next(message);
-    if (receiver) {
-        showMessage(message);
     }
-}
 
-function showMessage(message) {
-    addMessage(message);
-    document.getElementById('message').value = '';
-    scroll();
-}
+    function showMessage(message) {
+        addMessage(message);
+        document.getElementById('message').value = '';
+        scroll();
+    }
 
-document.getElementById('send').addEventListener('click', function(){
-    sendMessage(document.getElementById('message').value, username, false, username_friend);
-});
-document.getElementById('message').addEventListener("keyup", function(event) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
+    document.getElementById('send').addEventListener('click', function () {
         sendMessage(document.getElementById('message').value, username, false, username_friend);
+    });
+    document.getElementById('message').addEventListener("keyup", function (event) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.keyCode === 13) {
+            sendMessage(document.getElementById('message').value, username, false, username_friend);
+        }
+    });
+
+    document.getElementById('options').addEventListener('click', function () {
+        const profile = document.getElementById('profile');
+        profile.classList.toggle('expanded');
+        const contacts = document.getElementById('contacts');
+        contacts.classList.toggle('expanded');
+    });
+    document.getElementById('profile-img').addEventListener('click', function () {
+        const status = document.getElementById('status-options');
+        status.classList.toggle('active');
+    });
+
+
+    function addMessage(message) {
+        const messages = document.getElementById('messlist');
+        const li = document.createElement('li');
+        var text = document.createTextNode(message.content);
+        const p = document.createElement('p');
+        const image = document.createElement('img');
+        if (message.sender === username) {
+            li.classList.add('sent');
+            image.src = img;
+        } else {
+            li.classList.add("replies");
+            image.src = img2;
+        }
+        p.appendChild(text);
+        li.appendChild(image, li.childNodes[0]);
+        li.appendChild(p, li.childNodes[0]);
+        messages.appendChild(li, messages.childNodes[0]);
     }
-  });
 
-document.getElementById('options').addEventListener('click', function(){
-    const profile = document.getElementById('profile');
-    profile.classList.toggle('expanded');
-    const contacts = document.getElementById('contacts');
-    contacts.classList.toggle('expanded');
-});
-document.getElementById('profile-img').addEventListener('click', function(){
-    const status = document.getElementById('status-options');
-    status.classList.toggle('active');
-});
-
-
-function addMessage(message) {
-    const messages = document.getElementById('messlist');
-    const li = document.createElement('li');
-    var text = document.createTextNode(message.content);
-    const p = document.createElement('p');
-    const image = document.createElement('img');
-    if (message.sender === username) {
-        li.classList.add('sent');
-        image.src = img; 
-    } else {
-        li.classList.add("replies");
-        image.src = img2; 
+    function updateContact(message) {
+        const contacts = document.getElementById('contactlist');
+        const first_li = document.getElementsByClassName('contact-status')[0];
+        first_li.classList.add('online');
     }
-    p.appendChild(text);
-    li.appendChild(image, li.childNodes[0]);
-    li.appendChild(p, li.childNodes[0]);
-    messages.appendChild(li, messages.childNodes[0]);
-}
 
-function updateContact(message) {
-    const contacts = document.getElementById('contactlist');
-    const first_li = document.getElementsByClassName('contact-status')[0];
-    first_li.classList.add('online');
-}
-
-function scroll() {
-    setTimeout(() => {
-        scrollToBottom();
-    }, 100);
-}
-
-function getDiff() {
-    if (!viewer) {
-        return -1;
-    }
-    return viewer.scrollHeight - (viewer.scrollTop + viewer.clientHeight);
-}
-
-function scrollToBottom(t = 1, b = 0) {
-    if (b < 1) {
-        b = getDiff();
-    }
-    if (b > 0 && t <= 120) {
+    function scroll() {
         setTimeout(() => {
-            const diff = easeInOutSin(t / 120) * getDiff();
-            viewer.scrollTop += diff;
-            scrollToBottom(++t, b);
-        }, 1 / 60);
+            scrollToBottom();
+        }, 100);
     }
-}
 
-function easeInOutSin(t) {
-    return (1 + Math.sin(Math.PI * t - Math.PI / 2)) / 2;
-}
+    function getDiff() {
+        if (!viewer) {
+            return -1;
+        }
+        return viewer.scrollHeight - (viewer.scrollTop + viewer.clientHeight);
+    }
+
+    function scrollToBottom(t = 1, b = 0) {
+        if (b < 1) {
+            b = getDiff();
+        }
+        if (b > 0 && t <= 120) {
+            setTimeout(() => {
+                const diff = easeInOutSin(t / 120) * getDiff();
+                viewer.scrollTop += diff;
+                scrollToBottom(++t, b);
+            }, 1 / 60);
+        }
+    }
+
+    function easeInOutSin(t) {
+        return (1 + Math.sin(Math.PI * t - Math.PI / 2)) / 2;
+    }
+}());
--- server/server.js
+++ server/server.js
@@ -2,79 +2,118 @@ const server = require('http').createServer();
 const express = require('express');
 const path = require('path');
 const WebSocketServer = require('ws').Server;
-const { Subscription } = require('rxjs');
+const {Subscription} = require('rxjs');
 const cors = require('cors');
 
 const app = express();
 const PORT = 8081;
 
-const wss = new WebSocketServer({ server });
+const wss = new WebSocketServer({server});
 
 app.use(cors());
-//app.use(express.static(path.join(__dirname, '../browser')));
 
 let clientList = {};
-
+let cid = 0;
 // websocket on connection
 wss.on('connection', (client) => {
-  const subscription = new Subscription();
-  console.log(`New Client CONNECTED!`);
-  // send back to client to store clientID
-  client.send(createMessage(`connected_to_server`, false, 'SERVER'));
-  /* wss.clients.forEach(client2 => {
-        if (client2 != client) {
-            client2.send(createMessage(`new_client`, true, ''));
+    const clientId = cid++;
+    const subscription = new Subscription();
+    console.log(`New Client ${clientId} CONNECTED!`);
+    // send back to client to store clientID
+    client.send(createMessage({sender: 'SERVER', myClientId: clientId}, false, 'SERVER', '', 'INFO'));
+
+    // handle disconnect client
+    client.on('close', () => {
+        console.log(`client ${clientId} DISCONNECT`);
+        const client = saveClient(clientId, {type: 'disconnect'}, client);
+        sendMessage({type: 'ANNOUNCE', content: `user ${client.username} disconnected to channel`, sender: 'SERVER', userList: clientList.map(e => {
+                return {clientId: e.clientId, username: e.username, online: e.online};
+            })});
+        subscription.unsubscribe();
+    });
+
+    client.on('error', (error) => {
+        console.log(`client ${clientId} ERROR`);
+        console.error(error);
+        subscription.unsubscribe();
+    });
+
+    client.on('message', (msg) => {
+        let message;
+        console.log(`client ${clientId} -> ${msg}`);
+        try {
+            message = JSON.parse(msg);
+        } catch (err) {
+            console.error(`ERROR: client ${clientId} - unable to parse message "${msg}"`);
         }
-    }); */
-
-  client.on('close', () => {
-    console.log(`client CLOSED`);
-    subscription.unsubscribe();
-  });
-
-  client.on('error', (error) => {
-    console.log(`client ERROR`);
-    console.error(error);
-    subscription.unsubscribe();
-  });
-
-  client.on('message', (msg) => {
-    message = JSON.parse(msg);
-    console.log(`Message from client: ${message.sender} -> ${msg}`);
-    // save client on SERVER
-    if (!clientList[message.sender]) {
-      saveClient(message.sender, client);
-    }
-
-    if (message.receiver) {
-      // send message between two client
-      clientList[message.receiver].send(msg);
-    }
-
-    //ws.send(createMessage('Hi there, I am a WebSocket server', false, clientId));
-
-    if (message.isBroadcast) {
-        //send back the message to the other clients
-        wss.clients
-            .forEach(client2 => {
-                if (client2 != client) {
-                    client2.send(createMessage(message.content, true, message.sender));
+        switch (message.type) {
+            case 'connect': {
+                // save client on SERVER
+                saveClient(clientId, message, client);
+                sendMessage({type: 'ANNOUNCE', content: `user ${message.sender} connected to channel`, sender: 'SERVER', userList: clientList.map(e => {
+                        return {clientId: e.clientId, username: e.username, online: e.online};
+                    })});
+                break;
+            }
+            case 'disconnect': {
+                saveClient(clientId, message, client);
+                sendMessage({
+                    type: 'ANNOUNCE',
+                    content: `user ${message.sender} disconnected to channel`,
+                    sender: 'SERVER',
+                    userList: clientList.map(e => {
+                        return {clientId: e.clientId, username: e.username, online: e.online};
+                    })
+                });
+                break;
+            }
+            case 'message': {
+                console.log(`Message from client ${clientId} -> ${message.sender}: ${message}`);
+                //send back the message to the other clients
+                let toClient = null;
+                if (message.receiver) {
+                    toClient = clientList.find(e => e.username === message.receiver);
                 }
-            });
-    }
-
-  });
+                sendMessage(message, clientId, toClient);
+                break;
+            }
+        }
+    });
 });
 
 // create send message
-function createMessage(content, isBroadcast, sender, receiver) {
-    return JSON.stringify({content, isBroadcast, sender, receiver});
+function createMessage(content, isBroadcast, sender, receiver = 'all', type = 'MSG') {
+    return JSON.stringify({message: content, isPrivate: !isBroadcast, from: sender, to: receiver, type: type});
 }
 
 // save clients on SERVER
-function saveClient(username, client) {
-  console.log(`Client *${username}* saved on Server`);
-  clientList[username] = client;
+function saveClient(cId, connectMessage, client) {
+    let newClient = {clientId: cId, ws: client, username: connectMessage.sender, avatar: connectMessage.avatar, online: connectMessage.type === 'connect'};
+    if (connectMessage.type === 'connect') {
+        console.log(`Client *${connectMessage.sender}* saved on Server`);
+        client.send(createMessage({sender: 'SERVER', userList: clientList.map(e => {
+                return {clientId: e.clientId, username: e.username, online: e.online};
+            })}, false, 'SERVER', connectMessage.sender, 'LIST'));
+    }
+    clientList[connectMessage.sender] = newClient;
+    return newClient;
+}
+
+// manage send message from->to client or broadcast to channel
+function sendMessage(message, fromClient, toClient) {
+    clientList
+        .filter(e => !!e.online)
+        .forEach(client2 => {
+            if(message.type && message.type === 'ANNOUNCE') {
+                client2.ws.send(createMessage(message.content, !message.receiver, message.sender, message.receiver, message.type));
+            } else {
+                if (toClient) {
+                    clientList[toClient.username].ws.send(createMessage(message.content, !message.receiver, message.sender, message.receiver, message.type));
+                } else {
+                    client2.ws.send(createMessage(message.content, !message.receiver, message.sender, message.receiver, message.type));
+                }
+            }
+        });
 }
 
 server.listen(PORT, () => console.log(`server listening on port ${PORT}`));
